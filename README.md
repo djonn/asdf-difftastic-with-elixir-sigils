3075c2fe947f928dfe53532d863822b7ec765ecb
Jonas Nielsen <djonnneller@gmail.com>
2025-01-22 20:13:05 +0100

    Update script to build from source

--- README.md
+++ README.md
@@ -1,57 +1,9 @@
-<div align="center">
+# DO NOT USE THIS PLUGIN
 
-# asdf-difftastic [![Build](https://github.com/volf52/asdf-difftastic/actions/workflows/build.yml/badge.svg)](https://github.com/volf52/asdf-difftastic/actions/workflows/build.yml) [![Lint](https://github.com/volf52/asdf-difftastic/actions/workflows/lint.yml/badge.svg)](https://github.com/volf52/asdf-difftastic/actions/workflows/lint.yml)
+This is a bastardised ASDF plugin. Do not use it. Instead use the "real one": [asdf-difftastic](https://github.com/volf52/asdf-difftastic/).
 
-[difftastic](https://github.com/Wilfred/difftastic) plugin for the [asdf version manager](https://asdf-vm.com).
+This installs a specific non-official version of [difftastic](https://github.com/Wilfred/difftastic) specifically [the PR that improves diffing Elixir sigils](https://github.com/Wilfred/difftastic/pull/785).
 
-</div>
+This plugin is ONLY able to install that specific version of difftastic.
 
-# Contents
-
-- [Dependencies](#dependencies)
-- [Install](#install)
-- [Contributing](#contributing)
-- [License](#license)
-
-# Dependencies
-
-- `bash`, `curl`, `tar`: generic POSIX utilities
-
-# Install
-
-Plugin:
-
-```shell
-asdf plugin add difftastic
-# or
-asdf plugin add difftastic https://github.com/volf52/asdf-difftastic.git
-```
-
-difftastic:
-
-```shell
-# Show all installable versions
-asdf list-all difftastic
-
-# Install specific version
-asdf install difftastic latest
-
-# Set a version globally (on your ~/.tool-versions file)
-asdf global difftastic latest
-
-# Now difftastic commands are available
-difft --version
-```
-
-Check [asdf](https://github.com/asdf-vm/asdf) readme for more instructions on how to
-install & manage versions.
-
-# Contributing
-
-Contributions of any kind welcome! See the [contributing guide](contributing.md).
-
-[Thanks goes to these contributors](https://github.com/volf52/asdf-difftastic/graphs/contributors)!
-
-# License
-
-See [LICENSE](LICENSE) © [Arslan](https://github.com/volf52/)
+During installation Difftastic is built from source using Docker.
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,122 +2,65 @@
 
 set -euo pipefail
 
-GH_REPO="https://github.com/Wilfred/difftastic"
-TOOL_NAME="difftastic"
-TOOL_TEST="difft --version"
+TOOL_NAME="asdf-difftastic-with-elixir-sigils"
+TOOL_TEST="difft"
 
 fail() {
   echo -e "asdf-$TOOL_NAME: $*"
   exit 1
 }
 
-curl_opts=(-fsSL)
-
-# Will be active only in CI
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
-sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
-list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//'
+ensure_dependencies() {
+  test -x "$(which docker)" || fail "Requires docker for building difftastic from source"
 }
 
 list_all_versions() {
-  list_github_tags
-}
-
-get_latest_version() {
-  local latest_version
-  echo "Checking latest version for difftastic..." >&2
-
-  latest_version=$(curl -L --silent "${curl_opts[@]}" "https://api.github.com/repos/Wilfred/difftastic/releases/latest" |
-    grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/')
-  echo "Latest version for difftastic is $latest_version" >&2
-
-  echo "$latest_version"
-}
-
-download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
-
-  if [ "$version" = "latest" ]; then
-    version=$(get_latest_version)
-  fi
-
-  url="$GH_REPO/releases/download/${version}/${filename}"
-
-  echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$release_file" -C - "$url" || fail "Could not download $url"
-}
-
-get_arch() {
-  arch=$(uname -m | tr '[:upper:]' '[:lower:]')
-
-  # releases use "aarch64" instead of "arm64"
-  case $arch in
-  arm64)
-    arch='aarch64'
-    ;;
-  esac
-
-  echo "$arch"
-}
-
-get_platform() {
-  plat=$(uname | tr '[:upper:]' '[:lower:]')
-
-  case $plat in
-  darwin)
-    plat='apple-darwin'
-    ;;
-  linux)
-    plat='unknown-linux-gnu'
-    ;;
-  windows)
-    plat='pc-windows-msvc'
-    ;;
-  esac
-
-  echo "$plat"
+  echo "0.0.1"
 }
 
 install_version() {
   local install_type="$1"
-  local version="$2"
-  local install_path="${3%/bin}/bin"
+  local install_path="${2%/bin}/bin"
+  local build_path="$(mktemp -d -t difftastic-djonn.XXXXXXXXXX)"
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-$TOOL_NAME supports release installs only"
   fi
 
-  arch="$(get_arch)"
-  platform="$(get_platform)"
-  local release_file="difft-$arch-$platform.tar.gz"
+  ensure_dependencies
 
   (
     mkdir -p "$install_path"
-    # cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
-    download_release "$version" "$release_file"
-    tar -xzf "$release_file" -C "$install_path" || fail Could not extract "$release_file"
-    rm "$release_file"
+    mkdir -p "$build_path"
 
-    local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+    clone_repo "$build_path"
+    build_with_docker "$build_path" "$install_path"
+  
+    echo "##################  tool test:"
+    echo "$install_path/$TOOL_TEST"
+    test -x "$install_path/$TOOL_TEST" || fail "Expected $install_path/$TOOL_TEST to be executable."
 
-    echo "$TOOL_NAME $version installation was successful!"
+    echo "$TOOL_NAME installation was successful!"
   ) || (
     rm -rf "$install_path"
-    fail "An error occurred while installing $TOOL_NAME $version."
+    echo "$install_path"
+    fail "An error occurred while installing $TOOL_NAME."
   )
+}
+
+clone_repo() {
+  local build_path=$1
+  git clone --depth 1 --branch "improve-elixir-diffing" --single-branch git@github.com:crbelaus/difftastic.git "$build_path"
+}
+
+build_with_docker() {
+  local build_path=$1
+  local install_path=$2
+
+  docker run --rm --volume="$build_path:/build" --workdir="/build" rust:1.66 cargo build --release --bin="difft"
+
+  echo "##################  copy artifact:"
+  echo "$build_path/target/release/difft"
+  echo "$install_path/difft"
+  cp "$build_path/target/release/difft" "$install_path/difft"
 }
